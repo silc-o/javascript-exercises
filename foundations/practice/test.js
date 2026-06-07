25bb63ce7bc01731662ff96422ed5afec025bb50
silc-o <bhussienaldondie@gmail.com>
2026-06-07 19:03:29 +0800

    nothing

---
+++ foundations/practice/test.js
@@ -0,0 +1,15 @@
+function add7(num) {
+  return num + 7;
+}
+
+function multiply(num, num2) {
+  return num * num2;
+}
+
+function capitalize(str) {
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+function lastLetter(str) {
+  return str[str.length - 1];
+}
