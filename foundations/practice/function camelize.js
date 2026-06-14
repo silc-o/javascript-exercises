61c28fe294ad4d8c6eadc02fb2d70632726bf454
silc-o <bhussienaldondie@gmail.com>
2026-06-14 17:54:31 +0800

    Implement string manipulation functions: update repeatString, reverseString, and leapYears; add removeFromArray; create filterRange function

---
+++ foundations/practice/function camelize.js
@@ -0,0 +1,8 @@
+function filterRange(arr, a, b) {
+  return arr.filter(item => (item >= a && item <= b));
+}
+
+let arr = [5, 3, 8, 1];
+let filtered = filterRange(arr, 1, 4);
+alert(filtered);
+alert(arr);
